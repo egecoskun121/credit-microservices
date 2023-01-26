2efc13f065c014c11af97d11da5c4774c27c75d7
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2023-01-26 08:37:53 +0300

    Added birth date field

--- credit-service/src/main/java/com/burakkolay/credit/model/entity/Applicant.java
+++ credit-service/src/main/java/com/burakkolay/credit/model/entity/Applicant.java
@@ -1,12 +1,15 @@
 package com.burakkolay.credit.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 
@@ -29,6 +32,9 @@ public class Applicant implements Serializable {
     private double monthlyIncome;
     @Column(unique = true)
     private String phoneNumber;
+    @JsonFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateOfBirth;
 
     private int creditRating;
 
